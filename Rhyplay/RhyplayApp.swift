c8c528923745a3ebfdfa2ac8120468638087ab9f
Wantonius D. Silverfangs <76421738+Blueguy51@users.noreply.github.com>
2023-11-14 14:16:34 +0700

    Initial commit

---
+++ Rhyplay/RhyplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  RhyplayApp.swift
+//  Rhyplay
+//
+//  Created by Timothyus Kevin Dewanto on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RhyplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
